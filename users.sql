0946d2c1c88a999f5c12d5991976de61600ca251
samir-bueno <samirbueno585@gmail.com>
2024-09-17 17:09:10 -0300

    modifique las tablas comunas y users para su correcto funcionamiento

--- users.sql
+++ users.sql
@@ -1,14 +1,13 @@
-CREATE TABLE users (
-    "ID" INTEGER NOT NULL,
-    "Name" VARCHAR,
-    "Lastname" VARCHAR,
-    "Email" VARCHAR,
-    "Password" BLOB,
-    "Address" VARCHAR,
-    "Phone_number" VARCHAR,
-    "Comuna_id" INTEGER,
-    "Rol_id" INTEGER,
-	PRIMARY KEY("ID" AUTOINCREMENT),
-    FOREIGN KEY("Comuna_id") REFERENCES "Comunas"("ID"),
-    FOREIGN KEY("Rol_id") REFERENCES "Rols"("ID")
+CREATE TABLE users (   
+    ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255),
+    Lastname VARCHAR(255),
+    Email VARCHAR(255),
+    Password BLOB,
+    Address VARCHAR(255),
+    Phone_number VARCHAR(255),
+    Comuna_id INTEGER,
+    Rol_id INTEGER,
+    FOREIGN KEY (Comuna_id) REFERENCES comunas(ID),
+    FOREIGN KEY (Rol_id) REFERENCES rols(ID)
 );
