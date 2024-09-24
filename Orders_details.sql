828fa8daff1ce1bc2a5e22352e6fd61f57d3c86f
samir-bueno <samirbueno585@gmail.com>
2024-09-24 16:53:23 -0300

    inserte algunos datos para el detalle de la orden

--- Orders_details.sql
+++ Orders_details.sql
@@ -7,3 +7,8 @@ CREATE TABLE IF NOT EXISTS order_detail (
    FOREIGN KEY(Order_ID) REFERENCES orders(ID) ON DELETE CASCADE ON UPDATE CASCADE,
    FOREIGN KEY(Product_ID) REFERENCES products(ID) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+INSERT INTO `order_detail`(`Order_ID`, `Product_ID`, `Quantity`, `Unit_price`)
+VALUES (1, 1, 3, 75000),
+       (2, 2, 2, 75000),
+       (3, 3, 2, 60000)
