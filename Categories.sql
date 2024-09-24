28d03134805e1f92e63848b17de3bef525d76630
KevinOrosco <kevinorosco411@gmail.com>
2024-09-24 15:57:21 -0300

    Inserte las categorias y modifique el codigo de products debido a un error en el codigo

--- Categories.sql
+++ Categories.sql
@@ -3,3 +3,8 @@ CREATE TABLE categories (
    Name VARCHAR(255) NOT NULL,
    Description TEXT
 );
+
+INSERT INTO `categories` (`ID`, `Name`, `Description`)
+VALUES 
+('1', 'Camiseta', NULL), 
+('2', 'Short', NULL);
