989c2916675d659667be98052a83baac11f80384
avendandoangeline77 <uba.avendanoangelina@gmail.com>
2024-09-24 16:40:45 -0300

    inserte el id del usuario y agregue los 4 estados en la columna Status

--- Orders.sql
+++ Orders.sql
@@ -5,3 +5,17 @@ CREATE TABLE IF NOT EXISTS orders (
    Status  VARCHAR(255) NOT NULL,
    FOREIGN KEY(User_ID) REFERENCES users(ID) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+ALTER TABLE orders
+DROP COLUMN Data
+
+ALTER TABLE orders
+ADD Date datetime
+
+INSERT INTO `orders`(`User_ID`, `Status`)
+VALUES (2,'in progress'),
+	   (1,'submitted'),
+       (5,'canceled'),
+       (6,'return');
+       
