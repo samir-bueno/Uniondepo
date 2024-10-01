1f324c9a4f84868d5e14d8f47025d32af386652a
KevinOrosco <kevinorosco411>
2024-10-01 16:15:52 -0300

    Agregue las sentencias crud de sql en Users

--- users.sql
+++ users.sql
@@ -17,3 +17,28 @@ INSERT INTO `users`(`NAME`, `Lastname`, `Email`, `PASSWORD`, `Address`, `Phone_n
 VALUES ('Carlos', 'Torres', 'carlTor@gmail.com', 'wpjfea34sdmv55n6cfd', 'Corrientes 1234', '11 5246-4523', 3, 1),
 	   ('Sebas', 'Palmera', 'palSeb@gmail.com', 'asdpepq3jr023ttt', 'Villa Sol 134', '11 5146-4493', 6, 3),
        ('Karla', 'Hoynoki', 'kaHo@gmail.com', '302hv0ewnelqn', 'Pol Sr. 714', '11 1209-9303', 10, 2);
+
+UPDATE users 
+SET NAME = PEPE 
+WHERE NAME == Sebas
+
+SELECT * FROM users
+
+SELECT * FROM users
+WHERE NAME == Carlos
+
+DELETE FROM users
+WHERE NAME == Carlos
+
+SELECT * FROM users
+ORDER BY asc
+
+SELECT * FROM users
+ORDER BY desc
+LIMIT 1 OFFSET 1
+
+SELECT * FROM users
+ORDER BY desc
+LIMIT 1
+
+SELECT count(ID) FROM users
